fcc99f7d6038e602a2c6738b727e7841c9ef6140
Mateusz <1b13ostalecki@gmail.com>
2019-05-16 17:36:45 +0200

    Add mouth detection

--- FaceDetection/Models/Model.cs
+++ FaceDetection/Models/Model.cs
@@ -49,24 +49,60 @@ namespace FaceDetection
 
         public void CzaryMary()
         {
-            //tutaj czarujemy ze zdjęciem photos[index]
-            CascadeClassifier haarFace = new CascadeClassifier("haarcascade_frontalface_default.xml");
-            CascadeClassifier haarEyes = new CascadeClassifier("haarcascade_eye.xml");
-
-            Image<Bgr, Byte> imageCV = new Image<Bgr, byte>(photos[index].Btm);
-            Mat mat = imageCV.Mat;
-            var faces = haarFace.DetectMultiScale(mat, 1.1, 10, new Size(20, 20));
             MCvScalar RED = new MCvScalar(0, 0, 255);
             MCvScalar GREEN = new MCvScalar(0, 255, 0);
+            MCvScalar BLUE = new MCvScalar(255, 0, 0);
+
+            //klasyfikatory
+            CascadeClassifier haarFace = new CascadeClassifier("haarcascade_frontalface_default.xml");
+            CascadeClassifier haarEyes = new CascadeClassifier("haarcascade_eye.xml");
+            CascadeClassifier haarMouth = new CascadeClassifier("haarcascade_smile.xml");
+
+            //tutaj bitmapa jest konwertowana na obiekt Image z opencv
+            Image<Bgr, Byte> imageCV = new Image<Bgr, byte>(photos[index].Btm);
+
+            // faces to jest kolekcja rectangli z twarzami, czyli współrzędne wszystkich twarz znalezionych na zdjęciu
+            var faces = haarFace.DetectMultiScale(imageCV, 1.1, 10, new Size(20, 20));
             foreach (var face in faces)
             {
-                //Console.WriteLine($"{face.Left} : {face.Top} \n {face.Width}x{face.Height}");
+                // to niżej po prostu rysuje kwadrat na prostokącie z twarzą
                 CvInvoke.Rectangle(imageCV, face, RED, 2);
+
+                //obrazek jest zawężany tylko do mordy, a potem w tym obszarze szuka oczu no i rysuje na nich zielone prostokąty
                 imageCV.ROI = face;
                 var eyes = haarEyes.DetectMultiScale(imageCV, 1.1, 10, new Size(20, 20));
-                foreach(var eye in eyes)
+                var smiles = haarMouth.DetectMultiScale(imageCV, 1.1, 20, new Size(20, 20));
+
+                var imageparts = new List<Image<Bgr, byte>>();
+
+                int i = 1;
+                foreach (var eye in eyes.Reverse())
                 {
+                    if (i > 2) break;
+                    var xd = eye;
                     CvInvoke.Rectangle(imageCV, eye, GREEN, 2);
+
+                    xd.X += face.X;
+                    xd.Y += face.Y;
+
+                    imageCV.ROI = xd;
+
+                    var ziomek = imageCV.HoughCircles(new Bgr(), new Bgr(), 60, 60);
+                    ziomek.Save(@"../../Changed Photos/chuj.jpeg");
+
+                    imageCV.Save(@"../../Changed Photos/" + photos[index].FileName + @"_eye" +i +".jpeg");
+
+                    imageCV.ROI = face;
+
+                    i++;
+
+                }
+                i = 1;
+                foreach (var smile in smiles)
+                {
+                    if (i > 1) break;
+                    CvInvoke.Rectangle(imageCV, smile, BLUE, 2);
+                    i++;
                 }
                 imageCV.Save(@"../../Changed Photos/" + photos[index].FileName + @"_changed.jpeg");
             }
